6477c7835cef5146af6c0d133176865f68b26431
Your Name <christofaokoro@gmail.com>
2022-10-31 17:20:50 +0100

    added some images and code to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="wrapper">
+      <div className="heading">
+        <img alt="profile-pic"/>
+        <b>Christopher Okoro</b>
+      </div>
+      <div className="link-section">
+        <a href="https://twitter.com/its_chriso"  >Twitter Link</a>
+        <a href="https://training.zuri.team/" id ="btn__zuri" >Zuri Team</a>
+        <a href="http://books.zuri.team" id ="books" >Zuri Books</a>
+        <a href="https://books.zuri.team"  id ="book__python">Python Books</a>
+        <a href="https://background.zuri.team" id ="pitch" >Background Check for Coders</a>
+        <a href="https://books.zuri.team/design-rules" id ="book__design" >Design Books </a>
+      </div>
+      <div className="media-section"></div>
+     </div>
+     <div className="footer"></div>
     </div>
   );
 }
