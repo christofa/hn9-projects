dafec980346c68aa33c1dff324531694acf9546d
Your Name <christofaokoro@gmail.com>
2022-11-05 01:21:59 +0100

    adding contact content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Data from './components/Data'
 import Footer from './components/Footer'
 import Contact from "./components/Contact";
-import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function Contact() {
   return (
     <div>
-      <h1>Hello</h1>
+      <div className="contact-header">
+        <h1>Contact Me</h1>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      </div>
+
+      <form>
+        <label>First Name
+        <input type="text" placeholder="Enter your first name"/>
+        </label>
+        <label>Last Name
+        <input type="text" placeholder="Enter your last name"/>
+        </label>
+        <label>Email
+        <input type="text" placeholder="yourname@email.com"/>
+        </label>
+        <label>Message 
+          <textarea 
+          placeholder="Send me a message and I'll reply you as soon as possible...">
+          </textarea>
+        </label>
+        <div>
+          <input type="checkbox"/>
+          <p>You agree to providing your data to Christopher who may contact you.</p>
+          
+        </div>
+      </form>
     </div>
-  )
+  );
 }
