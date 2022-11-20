7a80aa01a1f8614b091372f842a2db1f8fcced9f
Your Name <christofaokoro@gmail.com>
2022-11-20 13:23:36 +0100

    added some new stylesheets

---
+++ src/App1.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+import PropTypes from "prop-types";
+
+import "./App2.css";
+
+export const Settings = () => {
+  return (
+    <div className="container">
+      <div className="side-bar"></div>
+      <div className="wrapper">
+        <h1>Settings</h1>
+        <div className="nav-section">
+          <div>
+            <h2>Account Settings</h2>
+
+            <div className="nav-btns">
+              <button to>Edit Account</button> 
+              <button  to="/">Users(21)</button>
+              <button>All Stories</button>
+              <button>Subscriptions</button>
+            </div>
+          </div>
+          <div className="admin-btn">
+            <button>Add Admin</button>
+          </div>
+        </div>
+
+        <form className='form-section'>
+          <div className="form-name">
+            <label>
+              First Name <br />
+              <input type="text" placeholder="Mark"  className="inputs2"/>
+            </label>
+            <label>
+              Last Name <br />
+              <input type="text" placeholder="Essien" className="inputs2" />
+            </label>
+          </div>
+
+          <label>
+            City <br />
+            <input type="text" placeholder="San Francisco" className="city"  />
+          </label>
+
+          <div className="region-section">
+            <label>
+              State <br />
+              <input type="text" placeholder="california" className="inputs2"/>
+            </label>
+            <label>
+              Country <br />
+              <input type="text" placeholder="America" className="inputs2"/>
+            </label>
+          </div>
+        </form>
+
+        <div className="email-cover">
+          <div>
+            <p>Email Address</p>
+            <p>Your email address is marke@storyai.com</p>
+          </div>
+          <a href="#">Hide</a>
+        </div>
+
+        <div className="email-details">
+          <div className="mail-inputs">
+            <label>
+              New Email Address <br />
+              <input type="text" placeholder="America" className="inputs2"/>
+            </label>
+            <label>
+              Current Password <br />
+              <input type="text" placeholder="America" className="inputs2"/>
+            </label>
+          </div>
+
+          <div className="mail-btns">
+            <button className="btn-1">Cancel</button>
+            <button className="btn-2">Save</button>
+          </div>
+        </div>
+
+        <div className="footer">
+            <input type="checkbox"/> 
+          <div>
+            <b>Email</b> <br/>
+            <p>Only approved emails can function as admin.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+// These are the types of the props that are passed to the component
+
+Settings.propTypes = {};
+
+// These are their default values
+
+Settings.defaultProps = {};
+
+export default Settings;
---
+++ src/components/AcctInfo.js
@@ -0,0 +1,108 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "../App3.css";
+
+export const Info = () => {
+  return (
+    <div className="App1">
+      <div className="side-bar1"></div>
+      <div className="wrapper-div">
+        <h2 className="header">Account Information</h2>
+        <div className="cover-section">
+          <b>Edit profile</b>
+          <div className="profile">
+            <img alt="profile-pic" src="./im" />
+            <div>
+              <p>Ella James</p>
+              <p>EllaJames@gmail.com</p>
+            </div>
+          </div>
+          <form>
+            <div className="grid-section">
+              <label>
+                Full name <br />
+                <input type="text" placeholder="" className="inputs" />
+              </label>
+              <label>
+                Email address <br />
+                <input
+                  type="text"
+                  placeholder="enter email address"
+                  className="inputs"
+                />
+              </label>
+              <label>
+                Phone number <br />
+                <input
+                  type="text"
+                  placeholder="enter phone number"
+                  className="inputs"
+                />
+              </label>
+              <label>
+                Birthday <br />
+                <input type="text" placeholder="dd/mm/yy" className="inputs" />
+              </label>
+            </div>
+
+            <h2>Chnage Password</h2>
+            <div className="pass-section">
+              <label>
+                Current password <br />
+                <input
+                  type="text"
+                  placeholder="enter current password"
+                  className="inputs"
+                />
+              </label>
+              <label>
+                New password <br />
+                <input
+                  type="text"
+                  placeholder="enter new password"
+                  className="inputs"
+                />
+              </label>
+            </div>
+
+            <h2>Address</h2>
+            <div className="address-section">
+              <label>
+                Address <br />
+                <input
+                  type="text"
+                  placeholder="enter home address"
+                  className="inputs"
+                />
+              </label>
+              <label>
+                House no <br />
+                <input
+                  type="text"
+                  placeholder="enter house number"
+                  className="inputs"
+                />
+              </label>
+            </div>
+          </form>
+
+          <div className="btns">
+            <button className="btn1">Cancel</button>
+            <button className="btn2">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// These are the types of the props that are passed to the component
+
+Info.propTypes = {};
+
+// These are their default values
+
+Info.defaultProps = {};
+
+export default Info;
